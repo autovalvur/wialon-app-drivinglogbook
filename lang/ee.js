bede772319bd749cd56746c5f605b150c1a1060f
autovalvur <siim@carcops.ee>
2015-02-26 11:19:01 +0200

    Update and rename lang to lang/ee.js

---
+++ lang/ee.js
@@ -0,0 +1,125 @@
+TRANSLATIONS = {
+	
+	"Driving Logbook": "",
+
+	"Logbook": "",
+
+	"Unit": "",
+	
+	"From": "",
+	
+	"To": "",
+	
+	"OK": "",
+	
+	"Beginning": "",
+	
+	"End": "",
+	
+	"Duration": "",
+	
+	"Initial location": "",
+	
+	"Final location": "",
+
+	"Initial mileage": "",
+
+	"Final mileage": "",
+
+	"Mileage": "",
+	
+	"Driver": "",
+	
+	"User": "",
+	
+	"Last changes": "",
+	
+	"Trip status": "",
+	
+	"Business": "",
+	
+	"Personal": "",
+	
+	"Notes": "",
+	
+	"Page&nbsp;": "",
+	
+	"&nbsp;of&nbsp;": "",
+	
+	"Apply": "",
+	
+	"Displaying %d to %d of %d items": "",
+	
+	"List of units empty.": "",
+	
+	"Login error.": "",
+	
+	"Please select unit.": "",
+	
+	"Please select time interval.": "",
+	
+	"No data for selected interval.": "",
+
+    "km": "",
+
+    "mi": "",
+
+    "%d pages_1": "%d page",
+    "%d pages_2": "%d pages",
+    "%d pages_5": "%d pages",
+
+    "Start": "",
+
+    "Prev": "",
+
+    "Next": "",
+
+    "Print": "",
+
+    "Yesterday": "",
+    "Today": "",
+    "Week": "",
+    "Month": "",
+    "Custom": "",
+
+    "Sunday": "",
+    "Monday": "",
+    "Tuesday": "",
+    "Wednesday": "",
+    "Thursday": "",
+    "Friday": "",
+    "Saturday": "",
+
+    "January": "",
+    "February": "",
+    "March": "",
+    "April": "",
+    "May": "",
+    "June": "",
+    "July": "",
+    "August": "",
+    "September": "",
+    "October": "",
+    "November": "",
+    "December": "",
+
+    "Sun": "",
+    "Mon": "",
+    "Tue": "",
+    "Wed": "",
+    "Thu": "",
+    "Fri": "",
+    "Sat": "",
+
+    "Jan": "",
+    "Feb": "",
+    "Mar": "",
+    "Apr": "",
+    "Jun": "",
+    "Jul": "",
+    "Aug": "",
+    "Sep": "",
+    "Oct": "",
+    "Nov": "",
+    "Dec": ""
+}
